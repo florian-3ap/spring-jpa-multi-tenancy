3336be42c721137e0e95513821a3536c89dba2cd
Florian Näpflin <florian@3ap.ch>
2021-06-09 18:45:57 +0200

    Remove exclusion of FlywayAutoConfiguration

--- src/main/java/com/example/jpamultitenancy/JpaMultiTenancyApplication.java
+++ src/main/java/com/example/jpamultitenancy/JpaMultiTenancyApplication.java
@@ -2,9 +2,8 @@ package com.example.jpamultitenancy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 
-@SpringBootApplication(exclude = {FlywayAutoConfiguration.class})
+@SpringBootApplication
 public class JpaMultiTenancyApplication {
 
   public static void main(String[] args) {
